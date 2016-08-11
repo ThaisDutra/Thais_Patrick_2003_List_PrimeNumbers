31a354a675299c33e8eeb29341810aaac4831823
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2016-08-11 14:03:54 -0300

    Add files via upload

---
+++ Patrick_Thais_2003_LIst_PrimeNumbers/Patrick_Thais_2003_LIst_PrimeNumbers/List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrick_Thais_2003_LIst_PrimeNumbers
+{
+    class List
+    {
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 2; j <= a; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+
+                        break;
+                    }
+
+
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine("Primo:    " + i);
+                }
+
+                isPrime = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Patrick_Thais_2003_LIst_PrimeNumbers/Patrick_Thais_2003_LIst_PrimeNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Patrick_Thais_2003_LIst_PrimeNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Patrick_Thais_2003_LIst_PrimeNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90c69b09-00d9-48eb-94f1-e1105aee1c85")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
